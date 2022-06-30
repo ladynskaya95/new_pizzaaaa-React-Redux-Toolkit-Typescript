06a60c9a4f38bd22f77027b2ae4ee896a814a5fa
ladynskaya95 <feverrr27@gmail.com>
2022-06-30 15:12:37 +0300

    add ts configuration

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter} from "react-router-dom";
 import { Provider } from 'react-redux';
 
-import App from './App';
+import App from './App.tsx';
 
 import { store } from './redux/store';
 
